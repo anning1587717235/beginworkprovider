04bf2304d3c669f8667b131ac90d8672c787d1eb
136416 <1364167600@qq.com>
2018-05-04 19:00:19 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/jk/mapper/a.java

--- src/main/java/com/jk/mapper/a.java
+++ src/main/java/com/jk/mapper/a.java
@@ -2,7 +2,7 @@ package com.jk.mapper;
 
 public class a {
 
-    private final String aaa="abcqaiouy";
+    private final String aaa="abctyrqaiouy";
 
     public void sou(){
         System.out.println(aaa);

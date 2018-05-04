9f661ba8b26586c91ca969b2ad0cd85202c409c1
anning <1587717235@qq.com>
2018-05-04 17:46:03 +0800

    hahadaas

--- src/main/java/com/jk/mapper/a.java
+++ src/main/java/com/jk/mapper/a.java
@@ -1,7 +1,9 @@
 package com.jk.mapper;
 
 public class a {
-    private final String aaa="ssssss";
+
+    private final String aaa="aaaaa";
+
     public void sou(){
         System.out.println(aaa);
     }
